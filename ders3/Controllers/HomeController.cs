67f39f7d746e283f0adc64583831891abbe48a4f
emremala <emremala@hotmail.com>
2016-10-05 14:11:03 +0300

    unit

---
+++ ders3/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ders3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.GirisBasarisiz = false;
+
+            if(Request.HttpMethod == "POST")
+            {
+                if(Request.Form["username"] == "sa" && Request.Form["password"] == "1")
+                {
+                    ViewBag.GirisBasarisiz = true;
+                    return Redirect("/Panel/Index");
+                }
+            }
+            return View();
+        }
+    }
+}
